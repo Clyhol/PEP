646cb65d1fa9910ce6ecb01d801cd6e166b0e792
Christoffer <clyhol20@student.aau.dk>
2024-05-28 10:49:49 +0200

    changed the learning rate

--- NLP/GPT.py
+++ NLP/GPT.py
@@ -8,7 +8,7 @@ batch_size = 32  # how many independent sequences will we process in parallel?
 block_size = 8  # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
-learning_rate = 0.001
+learning_rate = 0.01
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 
